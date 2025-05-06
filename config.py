778bb7b87d8cf05afd39e58e8b24c0cf382581b1
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-06 12:24:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 import time
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "21786970 "))
+API_HASH = os.environ.get("API_HASH", " aa1eaa84080fdf706c5cb37a27d35e81")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7849794082:AAEhUjqhZS-YMrWKgWghLbZnivv3MnRR_Lo")
+ADMIN = int(os.environ.get("ADMIN", " 5944299635"))
 
 
 # Premium 4GB Renaming Client Config
@@ -14,13 +14,13 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "BQCPRuQAE4DEstPiqGR-YV3qsRMqg
 BOT_UPTIME  = time.time()
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "OnAnimeSeries")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002169251162"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "KMovieHubInHindi")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002575288146"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://OnAnimeSeries:OnAnimeSeries@filexonanimeseries.2lwf4gr.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "FileXOnAnimeSeries")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://mohammadirfan5227:uwkiGejZGyRLV3nP@cluster0.ub1brgf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "renameTensaibot")
 
 
 # Other Variables Config
